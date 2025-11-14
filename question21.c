51d5c545df668486a85bb2864ba25b73dd375e78
mslun <m.s.lun@tcu.edu>
2025-11-14 00:12:19 -0600

    Created program that takes input number of rows and prints a cented pyramid of stars with a nested for loop.

---
+++ question21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+	printf("Enter number of rows: ");
+	int n;
+	scanf("%d", &n);
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=n-i;j++){
+			printf(" ");
+		}
+		for(int k=0; k<=i;k++){
+
+			printf("* ") ;
+		}
+		printf("\n");
+	}
+}
